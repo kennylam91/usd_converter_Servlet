74655316ebcf9375250cecac94654114116006e1
kennylam91 <phamvanlam1991@gmail.com>
2019-07-01 15:15:37 +0700

    convert usd OK

--- src/main/java/UsdConverterServlet.java
+++ src/main/java/UsdConverterServlet.java
@@ -3,9 +3,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class UsdConverterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer rate = Integer.parseInt(request.getParameter("rate"));
+        Integer usd = Integer.parseInt(request.getParameter("usd"));
+        Integer vnd = rate * usd;
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Vnd= </h1>"+vnd);
+        writer.println("</html>");
+
 
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,20 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Converter</title>
   </head>
   <body>
-  $END$
+  <h1>USD Converter:</h1>
+  <form action="/convert" method="post">
+    <div>
+      <label for="rate">Rate</label>
+      <input type="text" name="rate" id="rate" value="22000">
+    </div>
+    <div>
+      <label for="usd">USD</label>
+      <input type="text" name="usd" id="usd">
+    </div>
+    <input type="submit" value="Convert">
+  </form>
   </body>
 </html>
